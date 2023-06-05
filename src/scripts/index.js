4fc7c91322bdaf959f020e83fb5fa9ec7fb3da63
AudioBF <audiobf@gmail.com>
2023-06-05 20:53:33 -0300

    refactoring the code

---
+++ src/scripts/index.js
@@ -0,0 +1,48 @@
+import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js"
+import { user } from "./services/user.js"
+import { repositories } from "./services/repositories.js"
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    getUserProfile(userName)
+})
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if (isEnterKeyPressed) {
+        getUserProfile(userName)
+    }
+})
+
+function getUserProfile(userName) {
+    user(userName).then(userData => {
+        let userInfo = `<div class="info">
+                        <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" />
+                        <div class= "data"?
+                            <h1>${userData.name ?? 'Não possui nome cadastrado'}</h1>
+                            <p>${userData.bio ?? 'Não possui bio cadastrado'}</p>
+                        </div>
+                        </div>`
+        document.querySelector('.profile-data').innerHTML = userInfo
+
+        getUserRepositories(userName)
+    })
+}
+
+function getUserRepositories(userName) {
+    repositories(userName).then(reposData => {
+        let repositoriesItens = ""
+
+        reposData.forEach(repo => {
+            repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`
+        });
+
+        document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
+                                                                <h2>Repositories</h2>
+                                                                <ul>${repositoriesItens}</ul>
+                                                            </div>`
+    })
+}
